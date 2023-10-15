9ce06cd1f842387692b8f87987e5143752d75348
wzz <ywc0008@naver.com>
2023-10-16 03:57:28 +0900

    setup

---
+++ src/firbase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHWV5uWfzOj932hg9u-NsAiXoTSncufMg",
+  authDomain: "x-reloaded.firebaseapp.com",
+  projectId: "x-reloaded",
+  storageBucket: "x-reloaded.appspot.com",
+  messagingSenderId: "242919711997",
+  appId: "1:242919711997:web:c22c69df8c6ad0da0dca94"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/routes/login.tsx
@@ -0,0 +1,3 @@
+export default function Login() {
+  return <h1>create account</h1>;
+}
